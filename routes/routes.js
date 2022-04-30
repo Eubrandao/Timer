4926843cc5e7b196473893db0578d47c1bbbf1d4
Wellington Willians Barbieri Brandão <wellington.brandao@simbiox.com.br>
2022-04-30 10:29:03 -0300

    Signin Created

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import signupRoute from "./signupRoute.js";
+import signinRoute from "./SigninRoute.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(404).send({ msg: "Ops, caminho errado!" });
   });
 
-  app.use(express.json(), signupRoute);
+  app.use(express.json(), signupRoute, signinRoute);
 };
 
 export default routes;
